4beaa3de6933de2ee21173cd646f345a6cf6ef8c
Daniel Brockman <daniel@brockman.se>
2012-09-26 17:10:00 +0200

    Move pxorder.rb into payex/api subdirectory

--- lib/payex.rb
+++ lib/payex.rb
@@ -1,28 +1,28 @@
 module PayEx
   extend self
 
+  class Error < StandardError; end
+
   TEST_URL = 'https://test-external.payex.com'
   LIVE_URL = 'https://external.payex.com'
 
-  class Error < StandardError; end
-
-  attr_accessor :default_currency
   attr_accessor :base_url
+
+  self.base_url = PayEx::TEST_URL
 
   attr_accessor :account_number
   attr_accessor :encryption_key
-end
+  attr_accessor :default_currency
 
-PayEx.base_url = PayEx::TEST_URL
+  def account_number!
+    account_number or fail 'Please set PayEx.account_number'
+  end
+
+  def encryption_key!
+    encryption_key or fail 'Please set PayEx.encryption_key'
+  end
+end
 
 require 'payex/api'
-require 'payex/pxorder'
+require 'payex/api/pxorder'
 require 'payex/credit_card_redirect'
-
-def PayEx.account_number!
-  PayEx.account_number or fail 'Please set PayEx.account_number'
-end
-
-def PayEx.encryption_key!
-  PayEx.encryption_key or fail 'Please set PayEx.encryption_key'
-end
